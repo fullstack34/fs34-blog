fd8d25b20d03fb2c9726455218b12defaf3f42cc
Cauan <cauanvictor0325@gmail.com>
2024-08-10 12:13:30 -0300

    Usando json-server

--- src/pages/NewPost.jsx
+++ src/pages/NewPost.jsx
@@ -1,27 +1,52 @@
 
+import { useState } from "react";
+
 export default function NewPost() {
 
-  // Criar um estado chamado title para guardar a informação do input do Titulo
-  // Criar um estado chamado content para guardar a informação do input do Conteudo
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [slug, setSlug] = useState('');
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    // Exibir em um console.log ou alert, as informações dos inputs que estão guardadas
-    // nos estados
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({
+        title,
+        content,
+        slug
+      })
+    };
+
+    let response = await fetch('http://localhost:3000/posts', options);
+    let dados = await response.json();
+
+    console.log(dados);
+  }
+
+  const handleTitle = (event) => {
+    let inputValue = event.target.value;
+    setTitle(inputValue);
+    setSlug(inputValue.replaceAll(' ', '-').toLowerCase());
   }
 
   return (
     <form action="" className="max-w-96 m-auto" onSubmit={handleSubmit}>
       <div className="flex flex-col gap-2">
         <label htmlFor="">Titulo</label>
-        {/* usar no onChange para adicionar a informação no estado title*/}
-        <input type="text" className="border w-full" />
+        <input type="text" value={title} className="border w-full" onChange={handleTitle} />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="">Slug</label>
+        <input type="text" className="border w-full" value={slug} onChange={(event) => setSlug(event.target.value)} />
       </div>
       <div className="flex flex-col gap-2">
         <label htmlFor="">Conteúdo</label>
-        {/* usar no onChange para adicionar a informação no estado content*/}
-        <textarea className="border w-full"></textarea>
+        <textarea className="border w-full" onChange={(event) => setContent(event.target.value)}></textarea>
       </div>
       <div>
         <button className="w-full bg-black text-white mt-5">Criar novo post</button>
