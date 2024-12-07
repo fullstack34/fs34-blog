8a956deb0cd2de300bcb6b284434d74bf6965567
Kelvys Moura de Paiva <Kelvys.Paiva@digitalcollege.internal>
2024-12-07 11:40:41 -0300

    Alterando titulo Tags para negrito

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -24,7 +24,7 @@ export default function Tags () {
   return (
     tagsList.length > 0 && (
       <div className="w-[350px]">
-        <h3 className="text-xl pb-3">Tags</h3>
+        <h3 className="text-xl pb-3 text-bold">Tags</h3>
         <ul className="flex gap-2 flex-wrap">
           {tagsList.map(tag => <TagItem name={tag.name} />)}
         </ul>
